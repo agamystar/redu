af7faab1ed465f505c146f4660ae3bd530ab5d2e
agamystar <orcl.masr@hotmail.com>
2022-02-19 20:39:45 +0200

    first commit

--- fend/src/App.vue
+++ fend/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1> Generate Pin codes </h1>
-    <div class="row">
-         <div class="form-group ">
-          <div class="col-sm-4">
+    <div class="row text-center p-3">
+         <div class="form-group">
+          <div class="col-sm-12">
             <input type="text" class="form-control"
                    placeholder="Enter Number of Pin codes"  v-model="num_pin_code" />
           </div>
-          <div class="col-sm-3">
+           <div class="col-sm-12">
+            <br/>
+           </div>
+          <div class="col-sm-12">
             <input type="button" class="btn btn-primary" value="Generate"  @click="getData"/>
           </div>
        </div>
@@ -37,14 +40,14 @@ export default {
       getData(){
         return axios({
           method:'get',
-          url:'http://127.0.0.1:8002/gen_pin?num_pin_code='+this.num_pin_code,
+          url:'http://127.0.0.1:8000/gen_pin?num_pin_code='+this.num_pin_code,
           data:this.num_pin_code
         })
             .then(response => {
                this.rows=response.data.data;
                this.data_loaded=true;
             }).catch(error => {
-              alert("Please set the right url in App.vue file");
+              alert("Please set the backend  url in App.vue file");
               return error.response;
             })  .finally(() => this.loading = false);
 
--- fend/src/main.js
+++ fend/src/main.js
@@ -2,3 +2,6 @@ import {createApp} from 'vue';
 import App from './App';
 const Vue=createApp(App)
 Vue.mount('#app');
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
